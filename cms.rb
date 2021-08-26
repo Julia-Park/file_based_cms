9eb0d0e4b7e07e42337f2913aa3ac0c759a15d96
Julia-Park <julia.ay.park@gmail.com>
2021-08-26 12:24:42 -0400

    Add content and list view

--- cms.rb
+++ cms.rb
@@ -5,8 +5,16 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
+configure do
+  set :erb, :escape_html => true
+end
+
 helpers do; end
 
+before do
+  @docs = Dir.children("data").select { |fname| File.ftype("data/#{fname}") == "file" }
+end
+
 get '/' do
-  'Getting started.'
+  erb :doc_list, layout: :layout
 end
