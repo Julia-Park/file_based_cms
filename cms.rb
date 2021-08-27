ca3e19b80909cfa9c51388a44d9bd0604319f024
Julia-Park <julia.ay.park@gmail.com>
2021-08-27 18:09:11 -0400

    Add links to access data

--- cms.rb
+++ cms.rb
@@ -5,17 +5,18 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
-configure do
-  set :erb, :escape_html => true
-end
-
 helpers do; end
 
+configure do
+  set :erb, :escape_html => true
+  set :public_folder, __dir__ + '/data'
+end
+
 before do
-  root = File.expand_path('..', __FILE__)
-  @docs = Dir.glob(root + '/data/*').map do |path|
-    File.basename(path)
-  end
+  @root = File.expand_path('..', __FILE__)
+  @docs = Dir.glob(@root + '/data/*').map do |path|
+    File.basename(path) if File.ftype(path) == 'file'
+  end.compact
 end
 
 get '/' do
